51dd21f8221a0de31016a9bdba684feb22996b2a
Marcio Pessoa <marcio.pessoa@sciemon.com>
2017-11-03 20:40:56 -0200

    Instalation script

--- enm2/auxiliar.ino
+++
@@ -1,6 +0,0 @@
-/* auxiliar.ino, envmon Mark II - Environment Monitor, Arduino auxiliar 
- *                                                     sketch file
- * 
- * Author: Márcio Pessoa <marcio.pessoa@sciemon.com>
- * Contributors: none
- */
